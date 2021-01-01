e28378334e8157eff48bc2dcfb3bb9a920e6474c
Jonathan Evans <39573756+JemRF@users.noreply.github.com>
2021-01-01 10:49:30 -0500

    Add files via upload

--- pep.h
+++ pep.h
@@ -13,6 +13,9 @@
 //#define DS18B20
 #define DS18B20_INTERVAL 300000 //5 minutes
 
+extern const char* smtp_corp;
+extern const char* smtp_password;
+extern const char* smtp_user;
 extern const char* ssid;
 extern const char* password;
 extern const char* pep_token;
@@ -26,7 +29,7 @@ void init_queue();
 uint8_t http_request(int PEP_Function, String opcode0, String opcode1, String opcode2, char buf[255]);
 void send_data_to_server(int PEP_Function, String opcode0, String opcode1, String opcode2);
 void rf_init();
-unsigned int sendEmail(String EmailTo, String EmailMessage, char smtpcorp[50], int port, String username, String password);
+unsigned int sendEmail(String EmailTo, String EmailMessage);
 void compileEmailAlert(String SensorNo, String RuleId);
 void setup_wired_sensors();
 void poll_wired_sensors();
