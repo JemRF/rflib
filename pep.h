5d03d045f4e4626619577f6a947657bdd21e6e82
Jonathan Evans <39573756+JemRF@users.noreply.github.com>
2021-01-06 17:53:26 -0500

    Update pep.h

--- pep.h
+++ pep.h
@@ -10,8 +10,8 @@
 #define SIREN_DELAY  30
 #define CHIME_PIN 2
 
-#define RF
-#define DS18B20
+//#define RF
+//#define DS18B20
 #define DS18B20_INTERVAL 300000 //5 minutes
 
 extern const char* smtp_corp;
